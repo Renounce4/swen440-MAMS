aadabcc5e64808165ac2513ab21150a2183a4a5d
Kevin Hannigan <kph1958@rit.edu>
2017-04-02 12:40:38 -0400

    Gimme the beat boys...

--- heartbeat_server.js
+++ heartbeat_server.js
@@ -26,9 +26,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(methodOverride('X-HTTP-Method-Override')); 
 
+
+var didWeGetABeat = false;
+
 app.get('/heartbeat', function(req, res){
-    console.log('Gottem');
+    console.log('lub-dub');
+    didWeGetABeat = true;
 });
+
+function checkForHeartbeat(){
+    setTimeout(function(){
+        if(!didWeGetABeat){
+            console.log('Oh no! We didn\'t get a heartbeat!');
+            //TODO restart the heartbeat server
+            
+        } else {
+            console.log('We got a beat, good.');
+            didWeGetABeat = false;
+            checkForHeartbeat();
+        }
+    }, 5000);
+}
+
+checkForHeartbeat();
 
 app.get('*', function(req, res){
     res.send('Twee hee hee');
